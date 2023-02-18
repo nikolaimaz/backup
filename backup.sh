6322e2a70c35aeaa423f38583958c7c15a89c64b
nikolaimaz <--global>
2023-02-19 00:24:48 +0100

    added yammlint

--- backup.sh
+++ backup.sh
@@ -5,65 +5,93 @@ update='git pull enter repository and branch!!!!!!!!!'
 push='git push -u origin master'
 path="/backup/test"
 
+####################Text color output####################
+clear='\033[0m'
+green='\033[0;32m'
+red='\033[0;31m'
+
+
 start(){
-        if [ -d "$(pwd)/backup" ] 
-        then
-        echo "Directory  exists."
-        cd $(pwd)/backup/test
-        $update
-        else
-                echo "Directory does not exists, creating <backup> directory in $(pwd)/backup "
-                mkdir "$(pwd)/backup"
-                cd $(pwd)/backup
-                $clone
-                echo please enter youre working email
-                read mail
-                cd test/
-                git config user.email "$mail"
-fi
+	if [ -d "$(pwd)/backup" ] 
+	then
+    	echo "Directory  exists."
+    	cd $(pwd)/backup/test
+    	$update
+	else
+		echo "Directory does not exists, creating <backup> directory in $(pwd)/backup "
+		sudo apt install yamllint -y
+		mkdir "$(pwd)/backup"
+		cd $(pwd)/backup
+		$clone
+		echo please enter youre working email
+		read mail
+		cd test/ ###### change name of dir to the name youre repository
+		git config user.email "$mail"
+	fi
 }
 
 updating(){
-        cd $(pwd)$path
-        git reset --hard
-        $update
+	cd $(pwd)$path
+	git reset --hard
+	$update
+	
+}
 
+testing(){
+	yamllint .
+	yamllint . > test.txt
+	yaml_test=$(cat test.txt| grep -o error |sort -u)
 }
 
 save(){
+	
+	cd $(pwd)$path
+	testing
+	if [[ "$yaml_test" == 'error' ]]
+	then 
+		echo -e "\n${red}You have error in yaml file,cancelling saving"
+		echo -e "please review yaml files${clear}\n"
+		rm test.txt
 
-        cd $(pwd)$path
-
-        git add *
-
-        git commit -m "$date"
-
+	elif [[ "$yaml_test" == "" ]]
+	then
+		echo -e "\n${green}everything is ok, saving and pushing${clear}\n"
+		rm test.txt
+		git add *
+		git commit -m "$date"
+		$push
+	else
+		echo 'else'
+	fi
 }
+####################END OF CONFIGS####################
+
+
+####################MAIN FUNCTION###################
 
 options(){
-        echo -e 'Choose option \n 1 Download files update  \n 2 Save changes and push to Gitlab \n 9 Install '
-        read answ
-        if [[ $answ == 1 ]]
-        then
-                updating
+	echo -e 'Choose option \n 1 Download files update \n\n 2 Save changes and push to Gitlab \n\n 9 Install\n'
+	read answ
+	if [[ $answ == 1 ]]
+	then
+		updating
 
-        elif [[ $answ == 2 ]]
-        then
-                save
-                $push
+	elif [[ $answ == 2 ]]
+	then
+		save
+		
 
-        elif [[ $answ == 9 ]]
-        then
-                start
-        else
+	elif [[ $answ == 9 ]]
+	then
+		start
+	else
  
-                echo "Ooops you have made mistake restarting script"
-                echo
-                options
+		echo "Ooops you have made mistake restarting script"
+		echo
+		options
 
-        fi
-
+	fi
+	
 }
 
 options
-
